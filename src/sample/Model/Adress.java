faffc53858e57c7cd136b34cb6c80698c033924f
FabriceBosshard <f.bosshard@bosshard.com>
2017-03-27 11:14:28 +0200

    dfs<

--- src/sample/Model/Adress.java
+++ src/sample/Model/Adress.java
@@ -1,7 +1,25 @@
 package sample.Model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Fabrice on 27.03.2017.
  */
 public class Adress {
+
+    String name;
+    String forname;
+    String birthdate;
+    String adress;
+    ArrayList<String> emails = new ArrayList<>();
+    ArrayList<String> telephonicadresses = new ArrayList<>();
+
+
+    public Adress(String name, String forname, String birthdate, String adress){
+        this.name = name;
+        this.forname = forname;
+        this.birthdate = birthdate;
+        this.adress = adress;
+    }
 }
